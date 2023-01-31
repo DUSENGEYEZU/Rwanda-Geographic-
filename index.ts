7b2d65f56e03842ca5451f44328b051aff818945
DUSENGEYEZU <53561876+DUSENGEYEZU@users.noreply.github.com>
2023-01-31 10:29:05 +0200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+import provincesJson from "./provinces.json";
+import districtsJson from "./districts.json";
+import sectorsJson from "./sectors.json";
+import cellsJson from "./cells.json";
+import villagesJson from "./villages.json";
+
+export {
+    provincesJson,
+    districtsJson,
+    sectorsJson,
+    cellsJson,
+    villagesJson
+}
